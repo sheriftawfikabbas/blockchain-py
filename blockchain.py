d2114d585f3727ed052243b6413cc496386e67a1
Sherif Abbas <53269434+sheriftawfikabbas@users.noreply.github.com>
2024-05-22 16:18:55 +1000

    Initial commit

---
+++ blockchain.py
@@ -0,0 +1,72 @@
+import hashlib
+import json
+
+
+class Block:
+    def __init__(self, timestamp, data, previousHash=""):
+        self.previousHash = previousHash
+        self.timestamp = timestamp
+        self.data = data
+        self.hash = self.calculateHash()
+
+    def calculateHash(self):
+        if isinstance(self.previousHash, str):
+            self.previousHash = self.previousHash.encode()
+        data = (
+            self.previousHash + self.timestamp.encode() + json.dumps(self.data).encode()
+        )
+        return hashlib.sha256(data).digest()
+    
+    
+
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.createGenesisBlock()]
+
+    def createGenesisBlock(self):
+        print("Creating genesis block")
+        return Block("2017-01-01", [], "0")
+
+    def getLatestBlock(self):
+        return self.chain[-1]
+
+    def addBlock(self, block):
+        block.previousHash = self.getLatestBlock().hash
+        block.hash = block.calculateHash()
+        self.chain += [block]
+        print("Added new block:", block.hash)
+
+    def isChainValid(self):
+        print("Checking chain validity..")
+        realGenesis = self.createGenesisBlock()
+
+        if realGenesis.hash != self.chain[0].hash:
+            print('Hash of genesis is not reproducible:',self.chain[0].hash)
+            return False
+
+        print('Chain has',len(self.chain),'blocks')
+        for i in range(1,len(self.chain)):
+            currentBlock = self.chain[i]
+            previousBlock = self.chain[i - 1]
+
+            if previousBlock.hash != currentBlock.previousHash:
+                print(
+                    "Hash of prev block not the same as previousHash of current block at block number",
+                    i,
+                )
+                return False
+
+            if currentBlock.hash != currentBlock.calculateHash():
+                print("Current block hash is not reproducible at block number", i)
+                return False
+
+        print("** Chain is valid **")
+        return True
+
+
+supercoin = Blockchain()
+supercoin.addBlock(Block("2023-01-01", "123"))
+supercoin.addBlock(Block("2023-01-02", "567"))
+
+print(supercoin.isChainValid())
